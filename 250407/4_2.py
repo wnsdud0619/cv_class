e72626bccf53dabbcdf1c0c8f9ec2c10ec141306
jykim <kjyangks@naver.com>
2025-04-01 09:21:13 +0900

    250407 homework commit

---
+++ 250407/4_2.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+img = cv.imread('/home/jykim/cv_class/soccer.jpg')          
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+canny1 = cv.Canny(gray, 50, 150)
+canny2 = cv.Canny(gray, 100, 200)
+
+cv.imshow('Original', gray)
+cv.imshow('Canny1', canny1)
+cv.imshow('Canny2', canny2)
+
+cv.waitKey()
+cv.destroyAllWindows()
